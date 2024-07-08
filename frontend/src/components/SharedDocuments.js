1960e98b66d959b21ecca2438ba69a5397a0e4dd
lavuchandu169 <82090789+lavuchandu169@users.noreply.github.com>
2024-07-08 17:23:47 +0100

    Update SharedDocuments.js

---
+++ frontend/src/components/SharedDocuments.js
@@ -0,0 +1,13 @@
+// src/components/SharedDocuments.js
+import React from "react";
+
+const SharedDocuments = () => {
+  return (
+    <div>
+      <h2>Shared Documents</h2>
+      {/* Content for Shared Documents */}
+    </div>
+  );
+};
+
+export default SharedDocuments;
