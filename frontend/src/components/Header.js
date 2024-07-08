53b67a060e76b135f077fcae167770940a77201b
techno <160623296+Anusha-1598@users.noreply.github.com>
2024-07-08 15:05:59 +0000

    Pending changes exported from your codespace

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Docs</h1>
+      <nav className="header-nav">
+        <ul>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
