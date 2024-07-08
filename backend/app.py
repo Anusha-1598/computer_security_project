8f15a5759cfd4f8856fced7e2197c0f9ce78004f
lavuchandu169 <82090789+lavuchandu169@users.noreply.github.com>
2024-07-08 21:30:13 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_cors import CORS
 
 app = Flask(__name__)
 
@@ -14,6 +15,7 @@ app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
+CORS(app)
 
 # Models
 class User(db.Model):
