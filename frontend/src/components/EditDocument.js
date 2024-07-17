400d128b13625916f6673ff7e8494fdf0083bc44
lavuchandu169 <82090789+lavuchandu169@users.noreply.github.com>
2024-07-17 19:57:06 +0100

    Update EditDocument.js

--- frontend/src/components/EditDocument.js
+++ frontend/src/components/EditDocument.js
@@ -2,46 +2,80 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { updateDocumentContent } from "../redux/documentsSlice";
 import "./EditDocument.css";
+import io from "socket.io-client";
 import { setLoginUser } from "../redux/loginSlice";
+const socket = io.connect("http://localhost:3001");
 
 const EditDocument = () => {
   const currentUser = useSelector((state) => state.login.loginUser);
+  // const currentUser = "chandu";
   const currentDocument = useSelector(
     (state) => state.documents.currentDocument
   );
   const dispatch = useDispatch();
   const [content, setContent] = useState("");
   const [acknowledgment, setAcknowledgment] = useState("");
+  const [roomMessage, setRoomMessage] = useState("");
+
+  const joinRoom = () => {
+    if (currentDocument.fileId !== "") {
+      socket.emit("join_room", {
+        fileId: currentDocument.fileId,
+        userId: currentUser,
+      });
+    }
+  };
+
+  const sendMessage = (content) => {
+    socket.emit("send_message", {
+      userId: currentUser,
+      fileId: currentDocument.fileId,
+      content: content,
+    });
+  };
 
   useEffect(() => {
-    fetch("http://127.0.0.1:3658/m1/593636-0-default/verifySession", {
+    socket.on("receive_message", (data) => {
+      if (data.roomMessage) {
+        setRoomMessage(data.roomMessage);
+      }
+      if (data.userId && data.newContent) {
+        setContent(data.newContent);
+      }
+    });
+  }, [socket]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/verifyCookie", {
       method: "POST",
+      credentials: "include",
     })
       .then((res) => {
         if (res.status === 200) {
           res.json().then((res) => {
             dispatch(setLoginUser(res.userId));
             if (currentDocument) {
-              fetch(
-                "http://127.0.0.1:3658/m1/593636-0-default/getDocumentContent",
-                {
-                  method: "POST",
-                  credentials: "include",
-                  headers: {
-                    "Content-Type": "application/json",
-                    Accept: "application/json",
-                    "Access-Control-Allow-Origin": "*",
-                  },
-                  body: JSON.stringify({
-                    userId: currentUser,
-                    fileId: currentDocument.fileId,
-                  }),
-                }
-              )
+              fetch("http://127.0.0.1:5000/getDocumentContent", {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  userId: currentUser,
+                  fileId: currentDocument.fileId,
+                }),
+              })
                 .then((res) => {
                   if (res.status === 200) {
                     res.json().then((res) => {
                       setContent(res.fileContent);
+                      joinRoom();
+                      //if join room doesnt work just place join room as callback here and make apio call in joinroom
+                    });
+                  } else {
+                    res.json().then((res) => {
+                      alert(res.message);
                     });
                   }
                 })
@@ -70,17 +104,39 @@ const EditDocument = () => {
 
   const handleContentChange = (e) => {
     setContent(e.target.value);
+    sendMessage(e.target.value);
   };
 
   const handleSave = () => {
-    dispatch(
-      updateDocumentContent({
+    fetch("http://127.0.0.1:5000/saveDocumentContent", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userId: currentUser,
+        content: content,
         fileId: currentDocument.fileId,
-        newContent: content,
-      })
-    );
-    setAcknowledgment("Document content saved successfully!");
-    setTimeout(() => setAcknowledgment(""), 3000);
+      }),
+    }).then((res) => {
+      if (res.status === 200) {
+        res.json().then((res) => {
+          dispatch(
+            updateDocumentContent({
+              fileId: currentDocument.fileId,
+              newContent: content,
+            })
+          );
+          setAcknowledgment(res.message);
+          setTimeout(() => setAcknowledgment(""), 3000);
+        });
+      } else {
+        res.json().then((res) => {
+          alert(res.message);
+        });
+      }
+    });
   };
 
   return (
@@ -98,6 +154,7 @@ const EditDocument = () => {
             <strong>Owner:</strong> {currentDocument.ownerName}
           </p>
         )}
+        {roomMessage.length !== 0 ? <p>{roomMessage}</p> : <></>}
         {currentDocument.sharedUsers && (
           <div>
             <strong>Shared With:</strong>
@@ -130,3 +187,5 @@ const EditDocument = () => {
 };
 
 export default EditDocument;
+
+// https://codesandbox.io/s/slate-react-rich-text-editor-fbqre?file=/src/App.js
