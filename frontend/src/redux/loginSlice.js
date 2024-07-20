7c6a00ad858eddca05dbf40b119103f559ff063e
Kvgopika3 <165472831+Kvgopika3@users.noreply.github.com>
2024-07-20 15:32:13 +0100

    Update loginSlice.js

--- frontend/src/redux/loginSlice.js
+++ frontend/src/redux/loginSlice.js
@@ -1,8 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  username: "",
-  password: "",
   loginUser: "",
   isLogin: false,
 };
@@ -11,20 +9,16 @@ const loginSlice = createSlice({
   name: "login",
   initialState,
   reducers: {
-    setLoginData: (state, action) => {
-      state[action.payload.name] = action.payload.value;
-    },
     setLoginUser: (state, action) => {
       state.loginUser = action.payload;
       state.isLogin = true;
     },
     setLogout: (state) => {
       state.isLogin = false;
+      state.loginUser = "";
     },
-    resetLoginForm: () => initialState,
   },
 });
 
-export const { setLoginData, resetLoginForm, setLoginUser, setLogout } =
-  loginSlice.actions;
+export const { setLoginUser, setLogout } = loginSlice.actions;
 export default loginSlice.reducer;
