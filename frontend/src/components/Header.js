929e3025a189316969fde9124bd2f450d6a91a60
Kvgopika3 <165472831+Kvgopika3@users.noreply.github.com>
2024-07-20 15:18:56 +0100

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,27 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 import { useSelector } from "react-redux";
-
 const Header = () => {
-  const loginData = useSelector((state) => state.login);
+  const loginUser = useSelector((state) => state.login.loginUser);
   return (
     <header className="header">
       <h1>Docs</h1>
-      {loginData.loginUser ? (
-        <></>
-      ) : (
-        <nav className="header-nav">
-          <ul>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
-      )}
+
+      <nav className="header-nav">
+        <ul>
+          {loginUser ? (
+            <>
+              <li>{loginUser}</li>
+              <li>
+                {" "}
+                <Link to="/logout">Logout</Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
     </header>
   );
 };
