892d4609670963dafcd8651ec0765edc1566b726
Kvgopika3 <165472831+Kvgopika3@users.noreply.github.com>
2024-07-20 15:19:57 +0100

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,31 +2,27 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import DOMPurify from "dompurify";
 import { useNavigate } from "react-router-dom";
-import {
-  setLoginData,
-  resetLoginForm,
-  setLoginUser,
-} from "../redux/loginSlice";
+import { setLoginUser } from "../redux/loginSlice";
 import FormInput from "./FormInput";
 import Button from "./Button";
 import "./Login.css";
 
 const Login = () => {
   const dispatch = useDispatch();
-  const loginData = useSelector((state) => state.login);
   const [errors, setErrors] = useState({});
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
   const [passwordVisible, setPasswordVisible] = useState(false);
   const navigate = useNavigate();
-  const handleChange = (e) => {
-    const sanitizedValue = DOMPurify.sanitize(e.target.value);
-    dispatch(setLoginData({ name: e.target.name, value: sanitizedValue }));
-  };
 
   useEffect(() => {
-    fetch("http://127.0.0.1:5000/verifyCookie", {
+    fetch("http://127.0.0.1:5000/verifyUser", {
       method: "POST",
-      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ user: localStorage.getItem("user") }),
     })
       .then((res) => {
         if (res.status === 200) {
@@ -38,16 +34,17 @@ const Login = () => {
         }
       })
       .catch((err) => {
-        alert(err.message);
+        console.log(err.message);
+        // alert(err.message);
       });
   }, []);
 
   const validate = () => {
     const errors = {};
-    if (!loginData.username.trim()) {
+    if (!username.trim()) {
       errors.username = "Username is required";
     }
-    if (!loginData.password) {
+    if (!password) {
       errors.password = "Password is required";
     }
     return errors;
@@ -61,27 +58,30 @@ const Login = () => {
       return;
     }
     try {
-      const response = await fetch("http://127.0.0.1:5000/login", {
+      fetch("http://127.0.0.1:5000/login", {
         method: "POST",
-        credentials: "include",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          username: loginData.username,
-          password: loginData.password,
+          username: username,
+          password: password,
         }),
+      }).then((res) => {
+        if (res.status === 200) {
+          res.json().then((res) => {
+            localStorage.setItem("user", res.user);
+            setMessage("Login successful!");
+            dispatch(setLoginUser(username));
+            let goToDashBoard = () => navigate("/dashboard");
+            goToDashBoard();
+          });
+        } else {
+          res.json((res) => {
+            setMessage(`Login failed: ${res.message}`);
+          });
+        }
       });
-      const data = await response.json();
-      if (response.ok) {
-        setMessage("Login successful!");
-        dispatch(setLoginUser(loginData.username));
-        dispatch(resetLoginForm());
-        let goToDashBoard = () => navigate("/dashboard");
-        goToDashBoard();
-      } else {
-        setMessage(`Login failed: ${data.message}`);
-      }
     } catch (error) {
       setMessage(`Login failed: ${error.message}`);
     }
@@ -96,16 +96,20 @@ const Login = () => {
           type="text"
           name="username"
           label="Username"
-          value={loginData.username}
-          onChange={handleChange}
+          value={username}
+          onChange={(e) => {
+            setUserName(e.target.value);
+          }}
           error={errors.username}
         />
         <FormInput
           type={passwordVisible ? "text" : "password"}
           name="password"
           label="Password"
-          value={loginData.password}
-          onChange={handleChange}
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
           error={errors.password}
           toggleVisibility={() => setPasswordVisible(!passwordVisible)}
           isPassword
