732aab92bd5686e2fd736df27f229060d6c59beb
Kvgopika3 <165472831+Kvgopika3@users.noreply.github.com>
2024-07-20 15:31:06 +0100

    Update ViewDocument.js

--- frontend/src/components/ViewDocument.js
+++ frontend/src/components/ViewDocument.js
@@ -3,15 +3,16 @@ import { useDispatch, useSelector } from "react-redux";
 import "./ViewDocument.css";
 import { setLoginUser } from "../redux/loginSlice";
 import io from "socket.io-client";
+import { useNavigate } from "react-router-dom";
 const socket = io.connect("http://localhost:3001");
 
 const ViewDocument = () => {
   const currentUser = useSelector((state) => state.login.loginUser);
-  // const currentUser = "chandu";
   const [content, setContent] = useState("");
   const currentDocument = useSelector(
     (state) => state.documents.currentDocument
   );
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const [roomMessage, setRoomMessage] = useState("");
 
@@ -36,6 +37,7 @@ const ViewDocument = () => {
     socket.on("receive_message", (data) => {
       if (data.roomMessage) {
         setRoomMessage(data.roomMessage);
+        sendMessage(content);
       }
       if (data.userId && data.newContent) {
         setContent(data.newContent);
@@ -44,8 +46,16 @@ const ViewDocument = () => {
   }, [socket]);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:5000/verifyCookie", {
+    if (!currentDocument) {
+      let goToDashboard = () => navigate("/dashboard");
+      goToDashboard();
+    }
+    fetch("http://127.0.0.1:5000/verifyUser", {
       method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ user: localStorage.getItem("user") }),
     })
       .then((res) => {
         if (res.status === 200) {
@@ -54,12 +64,11 @@ const ViewDocument = () => {
             if (currentDocument) {
               fetch("http://127.0.0.1:5000/getDocumentContent", {
                 method: "POST",
-                credentials: "include",
                 headers: {
                   "Content-Type": "application/json",
                 },
                 body: JSON.stringify({
-                  userId: currentUser,
+                  userId: currentDocument.ownerName,
                   fileId: currentDocument.fileId,
                 }),
               })
@@ -84,7 +93,7 @@ const ViewDocument = () => {
             }
           });
         } else {
-          let goToLogin = () => dispatch("/login");
+          let goToLogin = () => navigate("/login");
           goToLogin();
           dispatch(setLoginUser(""));
         }
