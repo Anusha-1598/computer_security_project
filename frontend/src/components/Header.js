079f12a134ee036c22c67220f3f9a1c7faa8340b
techno <160623296+Anusha-1598@users.noreply.github.com>
2024-07-21 20:04:18 +0000

    importing libraries

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,37 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 import { useSelector } from "react-redux";
-const Header = () => {
-  const loginUser = useSelector((state) => state.login.loginUser);
-  return (
-    <header className="header">
-      <h1>Docs</h1>
-
-      <nav className="header-nav">
-        <ul>
-          {loginUser ? (
-            <>
-              <li>{loginUser}</li>
-              <li>
-                {" "}
-                <Link to="/logout">Logout</Link>
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <Link to="/register">Register</Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-            </>
-          )}
-        </ul>
-      </nav>
-    </header>
-  );
-};
-
-export default Header;
