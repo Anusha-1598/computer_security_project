ce442525922b4cf488a8bc25009c325c7840c525
techno <160623296+Anusha-1598@users.noreply.github.com>
2024-07-21 22:26:05 +0000

    clicks

--- frontend/src/components/MyDocuments.js
+++ frontend/src/components/MyDocuments.js
@@ -61,3 +61,77 @@ const MyDocuments = () => {
       .catch((err) => {
         alert("Failed to fetch documents due to : \n" + err.message);
       });
+      // fetch("http://127.0.0.1:5000/verifyCookie", {
+    //   method: "POST",
+    //   credentials: "include",
+    // })
+    //   .then((res) => {
+    //     if (res.status === 200) {
+    //       res.json().then((res) => {
+    //         dispatch(setLoginUser(res.userId));
+    //         fetch("http://127.0.0.1:5000/getUserDocuments", {
+    //           method: "POST",
+    //           credentials: "include",
+    //           headers: {
+    //             "Content-Type": "application/json",
+    //           },
+    //           body: JSON.stringify({ userId: currentUser }),
+    //         })
+    //           .then((res) => {
+    //             if (res.status === 200) {
+    //               res.json().then((res) => {
+    //                 let fetchedDocuments = [...res.filesList];
+    //                 let l = [];
+    //                 for (var i = 0; i < fetchedDocuments.length; i++) {
+    //                   let su = [];
+    //                   for (
+    //                     var j = 0;
+    //                     j < fetchedDocuments[i].sharedUsers.length;
+    //                     j++
+    //                   ) {
+    //                     let obj = { ...fetchedDocuments[i].sharedUsers[j] };
+    //                     su.push({
+    //                       userId: obj.userId,
+    //                       permissions: obj.permission.split(","),
+    //                     });
+    //                   }
+    //                   fetchedDocuments[i].sharedUsers = [...su];
+    //                   l.push(fetchedDocuments[i]);
+    //                 }
+    //                 dispatch(setDocuments(l));
+    //               });
+    //             } else {
+    //               res.json().then((res) => {
+    //                 alert(res.message);
+    //               });
+    //             }
+    //           })
+    //           .catch((err) => {
+    //             alert("Failed to fetch documents due to : \n" + err.message);
+    //           });
+    //       });
+    //     } else {
+    //       let goToLogin = () => navigate("/login");
+    //       goToLogin();
+    //       dispatch(setLoginUser(""));
+    //     }
+    //   })
+    //   .catch((err) => {
+    //     alert(err.message);
+    //   });
+  }, [dispatch]);
+
+  const handleRenameClick = (doc) => {
+    setSelectedDocument(doc);
+    setIsRenamePopupOpen(true);
+  };
+
+  const handleDeleteClick = (doc) => {
+    setSelectedDocument(doc);
+    setIsDeletePopupOpen(true);
+  };
+
+  const handleShareClick = (doc) => {
+    setSelectedDocument(doc);
+    setIsSharePopupOpen(true);
+  };
