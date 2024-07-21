9b69d766498156949fb9d497b7d92a2ad77f6cd0
techno <160623296+Anusha-1598@users.noreply.github.com>
2024-07-21 20:12:42 +0000

    nav bar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -11,3 +11,19 @@ const Header = () => {
     {loginData.loginUser ? (
       <></>
     ) : ( 
+      <nav className="header-nav">
+          <ul>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+      </header>
+    );
+  };
+  
+  export default Header;
