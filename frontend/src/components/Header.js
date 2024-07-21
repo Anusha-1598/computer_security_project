f6eb1dd234f88c5d674cdd6a020f05a836ee149c
techno <160623296+Anusha-1598@users.noreply.github.com>
2024-07-21 20:05:28 +0000

    login

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,3 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 import { useSelector } from "react-redux";
+
+const Header = () => {
+  const loginData = useSelector((state) => state.login);
+  return (
+    
