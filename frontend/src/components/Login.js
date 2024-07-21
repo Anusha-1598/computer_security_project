d699d6777b563e6dec0b5d1d8cdce0bf47f2ce61
techno <160623296+Anusha-1598@users.noreply.github.com>
2024-07-21 20:39:37 +0000

    const

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -10,3 +10,18 @@ import {
 import FormInput from "./FormInput";
 import Button from "./Button";
 import "./Login.css";
+
+
+const Login = () => {
+  const dispatch = useDispatch();
+  const loginData = useSelector((state) => state.login);
+  const [errors, setErrors] = useState({});
+  const [message, setMessage] = useState("");
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    const sanitizedValue = DOMPurify.sanitize(e.target.value);
+    dispatch(setLoginData({ name: e.target.name, value: sanitizedValue }));
+  };
+
+  
