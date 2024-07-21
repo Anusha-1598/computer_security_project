28ea4241f6bda02d193c621cb5b9fdfd1ae1de93
techno <160623296+Anusha-1598@users.noreply.github.com>
2024-07-21 20:46:52 +0000

    useEffect

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -11,7 +11,6 @@ import FormInput from "./FormInput";
 import Button from "./Button";
 import "./Login.css";
 
-
 const Login = () => {
   const dispatch = useDispatch();
   const loginData = useSelector((state) => state.login);
@@ -24,4 +23,21 @@ const Login = () => {
     dispatch(setLoginData({ name: e.target.name, value: sanitizedValue }));
   };
 
-  
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/verifyCookie", {
+      method: "POST",
+      credentials: "include",
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          res.json().then((res) => {
+            dispatch(setLoginUser(res.userId));
+            let goToDashBoard = () => navigate("/dashboard");
+            goToDashBoard();
+          });
+        }
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  }, []);
