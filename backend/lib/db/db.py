bb7c6ad67e483c0e7644b60988a2c540df8d5562
Kvgopika3 <165472831+Kvgopika3@users.noreply.github.com>
2024-07-29 15:41:31 +0100

    Update db.py

--- backend/lib/db/db.py
+++ backend/lib/db/db.py
@@ -1,7 +1,7 @@
 import sqlite3
 from datetime import datetime
 from lib.utils.actions import generate_alphanumeric_code
-from lib.utils.cryptic import hash_string
+from lib.utils.cryptic import hash_string, encrypt_to_fixed_length_string, decrypt_from_fixed_length_string
 
 def connect_db():
     return sqlite3.connect('example.db')
@@ -15,18 +15,18 @@ def create_tables():
                         user_id TEXT PRIMARY KEY,
                         password_hash TEXT NOT NULL
                       )''')
-
-     # Create files table
+    
+    # Create files table
     cursor.execute('''CREATE TABLE IF NOT EXISTS files (
                         file_id TEXT PRIMARY KEY,
                         user_id TEXT NOT NULL,
                         date_created TEXT NOT NULL,
                         file_name TEXT NOT NULL,
-                        content TEXT NOT NULL,
+                        content BLOB NOT NULL,
                         FOREIGN KEY (user_id) REFERENCES users(user_id)
-                      )''') 
-
-# Create shared_files table
+                      )''')
+    
+    # Create shared_files table
     cursor.execute('''CREATE TABLE IF NOT EXISTS shared_files (
                         id TEXT PRIMARY KEY,
                         file_id TEXT NOT NULL,
@@ -36,18 +36,18 @@ def create_tables():
                         FOREIGN KEY (file_id) REFERENCES files(file_id),
                         FOREIGN KEY (owner_id) REFERENCES users(user_id),
                         FOREIGN KEY (receiver_id) REFERENCES users(user_id)
-                      )''')  
-
-# Create cookies table
+                      )''')
+    
+    # Create cookies table
     cursor.execute('''CREATE TABLE IF NOT EXISTS cookies (
                         identifier TEXT NOT NULL,
                         encrypted_data BLOB NOT NULL
                       )''')
     
     conn.commit()
-    conn.close() 
+    conn.close()
 
-    def register_user(username, password):
+def register_user(username, password):
     if check_username_exists(username):
         return {"body": {"message": "Username already exists"}, "status_code": 401}
     else:
@@ -57,17 +57,17 @@ def create_tables():
         cursor.execute('INSERT INTO users (user_id, password_hash) VALUES (?, ?)', (username, password_hash))
         conn.commit()
         conn.close()
-        return {"body": {"message": "Registered Successfully"}, "status_code": 200}    
+        return {"body": {"message": "Registered Successfully"}, "status_code": 200}
 
-        def check_username_exists(username):
+def check_username_exists(username):
     conn = connect_db()
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM users WHERE user_id = ?', (username,))
     user = cursor.fetchone()
     conn.close()
-    return user is not None    
+    return user is not None
 
-    def login_user(username, password):
+def login_user(username, password):
     password_hash = hash_string(password)
     conn = connect_db()
     cursor = conn.cursor()
@@ -77,11 +77,10 @@ def create_tables():
     if user:
         return {"body": {"message": "Login Successfully"}, "status_code": 200}
     else:
-        return {"body": {"message": "Invalid Credentials"}, "status_code": 401}  
+        return {"body": {"message": "Invalid Credentials"}, "status_code": 401}
 
-        def insert_encryption(key, value):
+def insert_encryption(key, value):
     try:
-        print("here")
         conn = connect_db()
         cursor = conn.cursor()
         cursor.execute('INSERT INTO cookies (identifier, encrypted_data) VALUES (?, ?)', (key, value))
@@ -89,9 +88,9 @@ def create_tables():
         conn.close()
         return True
     except:
-        return False   
+        return False
 
-        def get_encryption(key):
+def get_encryption(key):
     conn = connect_db()
     cursor = conn.cursor()
     cursor.execute('SELECT encrypted_data FROM cookies WHERE identifier = ?', (key,))
@@ -102,11 +101,11 @@ def create_tables():
     else:
         return {"signal": False}
 
-        def getDocuments(userId):
+def getDocuments(userId):
     conn = connect_db()
     cursor = conn.cursor()
     cursor.execute('SELECT file_id, file_name, date_created  FROM files WHERE user_id = ?', (userId,))
-    userDocuments = [{"fileId": row[0], "fileName": row[1], "dateCreated": row[2], "ownerName":userId} for row in cursor.fetchall()]
+    userDocuments = [{"fileId": row[0], "fileName": row[1], "dateCreated": row[2], "ownerName": userId} for row in cursor.fetchall()]
     
     for document in userDocuments:
         cursor.execute('SELECT receiver_id, permission FROM shared_files WHERE file_id = ?', (document["fileId"],))
@@ -116,7 +115,7 @@ def create_tables():
     conn.close()
     return {"body": {"message": "Documents fetched Successfully", "filesList": userDocuments}, "status_code": 200}
 
-    def getSharedDocuments(userId):
+def getSharedDocuments(userId):
     conn = connect_db()
     cursor = conn.cursor()
     cursor.execute('SELECT file_id, owner_id, permission FROM shared_files WHERE receiver_id = ?', (userId,))
@@ -134,7 +133,6 @@ def create_tables():
     conn.close()
     return {"body": {"message": "Documents fetched Successfully", "filesList": sharedDocuments}, "status_code": 200}
 
-
 def getDocumentContent(userId, fileId):
     conn = connect_db()
     cursor = conn.cursor()
@@ -142,11 +140,12 @@ def getDocumentContent(userId, fileId):
     fileContent = cursor.fetchone()
     conn.close()
     if fileContent:
-        return {"body": {"message": "Document fetched Successfully", "fileId": fileId, "fileContent": fileContent[0]}, "status_code": 200}
+        decrypted_content = decrypt_from_fixed_length_string(fileContent[0])
+        return {"body": {"message": "Document fetched Successfully", "fileId": fileId, "fileContent": decrypted_content}, "status_code": 200}
     else:
         return {"body": {"message": "File not found"}, "status_code": 401}
 
-        def deleteDocument(userId, fileId):
+def deleteDocument(userId, fileId):
     conn = connect_db()
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM files WHERE file_id = ? AND user_id = ?', (fileId, userId))
@@ -161,7 +160,7 @@ def getDocumentContent(userId, fileId):
         conn.close()
         return {"body": {"message": "File not found"}, "status_code": 401}
 
-        def renameDocument(userId, fileId, newFileName):
+def renameDocument(userId, fileId, newFileName):
     conn = connect_db()
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM files WHERE file_id = ? AND user_id = ?', (fileId, userId))
@@ -175,24 +174,24 @@ def getDocumentContent(userId, fileId):
         conn.close()
         return {"body": {"message": "File not found"}, "status_code": 401}
 
-  def newFile(userId, fileName, dateCreated):
+def newFile(userId, fileName, dateCreated):
     conn = connect_db()
     cursor = conn.cursor()    
     cursor.execute('SELECT * FROM users WHERE user_id = ?', (userId,))
     user = cursor.fetchone()
     if user:
         fileId = generate_alphanumeric_code()
-        content = ""
+        encrypted_content = encrypt_to_fixed_length_string("")["encrypted_data"]
         cursor.execute('INSERT INTO files (file_id, user_id, date_created, file_name, content) VALUES (?, ?, ?, ?, ?)', 
-                       (fileId, userId, dateCreated, fileName, content))
+                       (fileId, userId, dateCreated, fileName, encrypted_content))
         conn.commit()
         conn.close()
         return {"body": {"message": "File Created Successfully"}, "status_code": 200}
     else:
         conn.close()
-        return {"body": {"message": "User not found"}, "status_code": 401}      
+        return {"body": {"message": "User not found"}, "status_code": 401}
 
-  def updateFilePermissions(userId, fileId, sharedUsers):
+def updateFilePermissions(userId, fileId, sharedUsers):
     conn = connect_db()
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM files WHERE file_id = ? AND user_id = ?', (fileId, userId))
@@ -202,35 +201,33 @@ def getDocumentContent(userId, fileId):
         rejected_users = []
         for entry in sharedUsers:
             sharedUserId = entry["userId"]
-            cursor.execute('SELECT * FROM users WHERE user_id = ?',(entry["userId"],))
+            cursor.execute('SELECT * FROM users WHERE user_id = ?', (entry["userId"],))
             user = cursor.fetchone()
-            print(user)
             if user:
                 permission = entry["permission"]
                 cursor.execute('INSERT INTO shared_files (file_id, owner_id, receiver_id, permission) VALUES (?, ?, ?, ?)', 
-                           (fileId, userId, sharedUserId, permission))
+                               (fileId, userId, sharedUserId, permission))
             else:
-                print("rejected")
                 rejected_users.append(entry["userId"])
         conn.commit()
         conn.close()
         message = "File Permissions Updated Successfully"
-        if len(rejected_users)!=0:
+        if rejected_users:
             for userId in rejected_users:
-                message += "\nUserId {} not found".format(userId)
-        print(message, rejected_users)
-        return {"body": {"message": message, "rejectedUsers":rejected_users, "isRejected":True if len(rejected_users)!=0 else False}, "status_code": 200}
+                message += f"\nUserId {userId} not found"
+        return {"body": {"message": message, "rejectedUsers": rejected_users, "isRejected": bool(rejected_users)}, "status_code": 200}
     else:
         conn.close()
         return {"body": {"message": "File not found"}, "status_code": 401}
 
-        def saveDocumentContent(userId, fileId, content):
+def saveDocumentContent(userId, fileId, content):
     conn = connect_db()
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM files WHERE file_id = ? AND user_id = ?', (fileId, userId))
     file = cursor.fetchone()    
     if file:
-        cursor.execute('UPDATE files SET content = ? WHERE file_id = ? AND user_id = ?', (content, fileId, userId))
+        encrypted_content = encrypt_to_fixed_length_string(content)["encrypted_data"]
+        cursor.execute('UPDATE files SET content = ? WHERE file_id = ? AND user_id = ?', (encrypted_content, fileId, userId))
         conn.commit()
         conn.close()
         return {"body": {"message": "File updated Successfully"}, "status_code": 200}
@@ -240,7 +237,8 @@ def getDocumentContent(userId, fileId):
         if shared_file:
             permission = shared_file[0]
             if "edit" in permission.split(","):
-                cursor.execute('UPDATE files SET content = ? WHERE file_id = ?', (content, fileId))
+                encrypted_content = encrypt_to_fixed_length_string(content)["encrypted_data"]
+                cursor.execute('UPDATE files SET content = ? WHERE file_id = ?', (encrypted_content, fileId))
                 conn.commit()
                 conn.close()
                 return {"body": {"message": "File updated Successfully"}, "status_code": 200}
@@ -251,11 +249,10 @@ def getDocumentContent(userId, fileId):
             conn.close()
             return {"body": {"message": "File not found"}, "status_code": 401}
 
-            def logout(identifier):
+def logout(identifier):
     conn = connect_db()
     cursor = conn.cursor()
-    print(identifier)
-    cursor.execute('DELETE FROM cookies WHERE identifier = ?',(identifier,))
-    return {"body":{"message":"User logged out Successfully"},"status_code": 200}
-
-  
+    cursor.execute('DELETE FROM cookies WHERE identifier = ?', (identifier,))
+    conn.commit()
+    conn.close()
+    return {"body": {"message": "User logged out Successfully"}, "status_code": 200}
